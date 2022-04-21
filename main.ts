5caa619ac2e3cf4bb929f2ba8d74333c2a57a1ca
agarciaexposito <72792744+agarciaexposito@users.noreply.github.com>
2022-04-21 11:44:49 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-
+function parpadeo () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    parpadeo()
+})
+input.onButtonPressed(Button.B, function () {
+    parpadeo()
+})
